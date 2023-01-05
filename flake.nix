6b643ec104b26ac9abbc9c519608c8e44f033b16
Lainera <fistlain@hotmail.com>
2023-01-05 01:29:50 +0000

    feat: flake
    - Add yarn as dev dependency
    - Add flake & flake.lock
    - Update node to 19

---
+++ flake.nix
@@ -0,0 +1,45 @@
+{
+  description = "TS Server nix flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
+    flake-utils.url = "github:numtide/flake-utils";
+    dream2nix = {
+			url = "github:lainera/dream2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+		};
+    gitignore = {
+      url = "github:hercules-ci/gitignore.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, dream2nix, gitignore, ... }@inp:
+    let
+      npmPackage = dream2nix.lib.makeFlakeOutputs {
+        systems = flake-utils.lib.defaultSystems;
+        config.projectRoot = ./.;
+        source = gitignore.lib.gitignoreSource ./.;
+
+        projects = {
+          typescript-language-server = {
+            name = "typescript-language-server";
+            subsystem = "nodejs";
+            translator = "yarn-lock";
+						subsystemInfo.nodejs = "19";
+          };
+        };
+
+      };
+
+      deps = flake-utils.lib.eachDefaultSystem (system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        {
+          formatter = pkgs.nixpkgs-fmt;
+        });
+    in
+
+    nixpkgs.lib.recursiveUpdate npmPackage deps;
+}
